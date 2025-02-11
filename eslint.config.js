aab1545f34ed8fab2dfd48157ad76473cd7674a3
EranosyanEduard <EranosyanEduard@yandex.ru>
2025-02-11 20:22:36 +0300

    feat: создать js-проект

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import vitest from '@vitest/eslint-plugin'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    files: ['**/*.test.ts'],
+    languageOptions: { globals: vitest.environments.env.globals },
+    plugins: { vitest },
+    rules: vitest.configs.all.rules,
+    settings: { vitest: { typecheck: true } }
+  }
+]
